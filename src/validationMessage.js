befd336d4d47474ec007fdf2861915f3dc9fa61e
jatint17 <jatintakkar@ymail.com>
2021-04-19 22:57:28 +0530

    Components with useState and validations created

--- src/validationMessage.js
+++ src/validationMessage.js
@@ -1,11 +1,5 @@
-
 const validationMessage = {
-
-    ageSmallThanZero: "age can't be smaller than zero",
-    ageGreaterThan120:"age can't be greater than 120",
-    nameSmallthanLengthTwo: "name can't be smaller than 2 letters"
-
-
-}
+    userIdValidation: "Id cannot be negative"
+};
 
 export default validationMessage;
