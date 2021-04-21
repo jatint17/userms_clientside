49c309f2b91d261455c028f27ee0c34407603a4b
jatint17 <jatintakkar@ymail.com>
2021-04-21 23:06:07 +0530

    login page added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import Home from './component/Home';
-import AddProduct from './component/AddProduct'
-import AddAdmin from './component/AddAdmin'
-import AddCustomer from './component/AddCustomer'
-import FindProductById from './component/FindProductById';
-import DisplayProductDetails from './component/DisplayProductDetails';
-import DisplayUserDetails from './component/DisplayUserDetails';
-import FindCustomerById from './component/FindCustomerById';
-import FindUserById from'./component/FindUserById'
-import UpdatePrice from './component/UpdatePrice';
-import UpdateBalance from './component/UpdateBalance';
+import Login from './component/Login';
+// import AddProduct from './component/AddProduct'
+// import AddAdmin from './component/AddAdmin'
+// import AddCustomer from './component/AddCustomer'
+// import FindProductById from './component/FindProductById';
+// import DisplayProductDetails from './component/DisplayProductDetails';
+// import DisplayUserDetails from './component/DisplayUserDetails';
+// import FindCustomerById from './component/FindCustomerById';
+// import FindUserById from'./component/FindUserById'
+// import UpdatePrice from './component/UpdatePrice';
+// import UpdateBalance from './component/UpdateBalance';
 import commonStyle2 from './component/css/commonStyle2.module.css'
 
 function App() {
@@ -23,7 +23,7 @@ function App() {
 
                     <div className="col-md-9">
 
-                        <UpdatePrice/>
+                        <Login />
 
                     </div>
                 </div>
